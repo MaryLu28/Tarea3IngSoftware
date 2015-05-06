9cca78c81068ce3f6de9e4a7c50d31f4ad8fecb4
DaniOrtiz <danidaniortiz@gmail.com>
2015-05-06 10:30:43 -0430

    Creación Proyecto Billetera Electrónica

---
+++ BilleteraElectronica.py
@@ -0,0 +1,17 @@
+'''
+Created on 4/5/2015
+
+@author: Daniela Ortiz (10-10517)
+         Maria Lourdes Garcia (10-10264)
+'''
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
---
+++ testBilleteraElectronica.py
@@ -0,0 +1,19 @@
+'''
+Created on 4/5/2015
+
+@author: Daniela Ortiz (10-10517)
+         Maria Lourdes Garcia (10-10264)
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
